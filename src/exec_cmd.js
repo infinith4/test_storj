18ca2d9941d9405afe35da35efbd1098ac13621e
Hiroshi <infinith4@gmail.com>
2024-01-11 23:03:18 +0900

    Update exec_cmd.js

--- src/exec_cmd.js
+++ src/exec_cmd.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 var readline = require("readline");
 
 var storj_ls_json = {'file_list' : []};
-
+var local_files_upload_check_json = {'file_list' : []};
 cmd  = 'rclone ls storj:test-bucket'
 exec(cmd, (err, stdout, stderr) => {
   //exec('ls -l sample.txt', (err, stdout, stderr) => {
@@ -21,19 +21,29 @@ exec(cmd, (err, stdout, stderr) => {
       console.log(line.replace(/\s*\d+/, '').trim());
     }
   }
-  console.log(storj_ls_json);
+  console.log(`storj_ls_json: ${storj_ls_json}`);
+
+  const local_files = fs.readdirSync('upload_files');
+  console.log(`local_files: ${local_files}`);
+
+
+  for (const local_file_name of local_files) {
+    var stat = fs.statSync(`upload_files/${local_file_name}`);
+    console.log(`local file_name: ${local_file_name}; size: ${stat.size}`);
+    //console.log(storj_ls_json);
+    for (const storj_file of storj_ls_json.file_list) {
+      console.log(`${storj_file.file_name}, ${local_file_name} ${storj_file.file_size} ${stat.size}`)
+      if(storj_file.file_name == local_file_name && storj_file.file_size == stat.size ) {
+        console.log("uploaded");
+        local_files_upload_check_json.file_list.push(local_file_name);
+        break;
+      }
+    }
+  }
+
+  console.log(`local_files_upload_check_json.file_list: ${JSON.stringify(local_files_upload_check_json.file_list, null, 2)}`);
+
 });
-
-const local_files = fs.readdirSync('upload_files');
-console.log(local_files)
-
-// for (const local_filename of local_files) {
-//   for (const storj_filename of storj_files) {
-
-//     var stat = fs.statSync(`upload_files/${filename}`);
-//     console.log(`filename: ${filename}; size: ${stat.size}`);
-//   }
-// }
 
 // for (const filename of files) {
 //   exec(`rclone copy ./upload_files/${filename} storj:test-bucket/`, (err, stdout, stderr) => {
